7e64de4a387689d65ce8859aaacd83e22f9a0243
LoveSun-Knight <2971859500@qq.com>
2023-08-23 21:53:03 +0800

    neovide conf

---
+++ neovide/neovide.lua
@@ -0,0 +1,35 @@
+-- 鼠标
+vim.o.mouse = "a"
+-- -- 行间距
+-- vim.opt.linespace = 2
+-- -- 缩放
+-- vim.g.neovide_scale_factor = 0.9
+-- 填充
+vim.g.neovide_padding_top = 25
+vim.g.neovide_padding_bottom = 25
+vim.g.neovide_padding_right = 25
+vim.g.neovide_padding_left = 25
+-- 没有空闲
+vim.g.neovide_no_idle = true
+-- 退出需要确认
+vim.g.neovide_confirm_quit = true
+-- 是否全屏
+vim.g.neovide_fullscreen = false
+-- 记住以前窗口的大小
+vim.g.neovide_remember_window_size = true
+-- 使用super键位,比如<cmd>
+-- vim.g.neovide_input_use_logo = true
+-- 开启Alt和Meta按键
+vim.g.neovide_input_macos_alt_is_meta = false
+-- 开启光标粒子
+vim.g.neovide_cursor_vfx_mode = "railgun"
+-- vim.g.neovide_cursor_vfx_mode = "ripple"
+vim.g.neovide_cursor_vfx_particle_density = 20.0
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
+
+vim.o.guifont = "Hack Nerd Font:h14"
+
+vim.g.neovide_refresh_rate = 144
+
+vim.g.neovide_cursor_animate_in_insert_mode = true
