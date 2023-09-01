312f07ced4f10a9acfc52efc1a7e93dd8b6fbbdf
bingo-h <2971859500@qq.com>
2023-09-01 18:30:33 +0800

    laptop config

--- neovide/neovide.lua
+++ neovide/neovide.lua
@@ -3,12 +3,12 @@ vim.o.mouse = "a"
 -- -- 行间距
 -- vim.opt.linespace = 2
 -- -- 缩放
--- vim.g.neovide_scale_factor = 0.9
+vim.g.neovide_scale_factor = 0.9
 -- 填充
-vim.g.neovide_padding_top = 25
-vim.g.neovide_padding_bottom = 25
-vim.g.neovide_padding_right = 25
-vim.g.neovide_padding_left = 25
+-- vim.g.neovide_padding_top = 0
+-- vim.g.neovide_padding_bottom = 0
+-- vim.g.neovide_padding_right = 0
+-- vim.g.neovide_padding_left = 0
 -- 没有空闲
 vim.g.neovide_no_idle = true
 -- 退出需要确认
@@ -16,7 +16,7 @@ vim.g.neovide_confirm_quit = true
 -- 是否全屏
 vim.g.neovide_fullscreen = false
 -- 记住以前窗口的大小
-vim.g.neovide_remember_window_size = true
+vim.g.neovide_remember_window_size = false
 -- 使用super键位,比如<cmd>
 -- vim.g.neovide_input_use_logo = true
 -- 开启Alt和Meta按键
@@ -28,8 +28,8 @@ vim.g.neovide_cursor_vfx_particle_density = 20.0
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
 
-vim.o.guifont = "Hack Nerd Font:h14"
+vim.o.guifont = "Hack Nerd Font:h12"
 
-vim.g.neovide_refresh_rate = 144
+vim.g.neovide_refresh_rate = 60
 
 vim.g.neovide_cursor_animate_in_insert_mode = true
