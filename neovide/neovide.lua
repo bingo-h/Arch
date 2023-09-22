1a39587ddb3fc7d4ab4d7374c1faf1979e88c4dc
Bingo <81412146+bingo-h@users.noreply.github.com>
2023-09-22 11:46:14 +0800

    Update neovide.lua

--- neovide/neovide.lua
+++ neovide/neovide.lua
@@ -22,7 +22,7 @@ vim.g.neovide_remember_window_size = true
 -- 开启Alt和Meta按键
 vim.g.neovide_input_macos_alt_is_meta = false
 -- 开启光标粒子
-vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_mode = "wireframe"
 -- vim.g.neovide_cursor_vfx_mode = "ripple"
 vim.g.neovide_cursor_vfx_particle_density = 20.0
 vim.g.neovide_cursor_animate_command_line = true
